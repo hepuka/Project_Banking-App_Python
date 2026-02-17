d71040c1b4aa30291e011a4218267846cf1af5ad
kun-fagyal_zoltan <kunzoltan7707@gmail.com>
2026-02-17 23:01:33 +0100

    upd

---
+++ services/helper_service.py
@@ -0,0 +1,36 @@
+import random
+
+class Helpers:
+
+    @staticmethod
+    def generate_account_number():
+        first_block = "1177" + str(random.randint(1000, 9999))
+        second_block = str(random.randint(10_000_000, 99_999_999))
+        third_block = str(random.randint(10_000_000, 99_999_999))
+        return f"{first_block}-{second_block}-{third_block}"
+
+    @staticmethod
+    def format_amount(amount) -> str:
+        return f"{int(round(amount)):,}".replace(",", ".")
+
+    @staticmethod
+    def print_table(title: str, rows: list[tuple[str, str]]):
+        col_width = max(len(label) for label, _ in rows) + 2
+
+        print(f"\n{title}")
+
+        for label, value in rows:
+            print(f"{label.ljust(col_width)} | {value}")
+
+    @staticmethod
+    def get_positive_int(prompt: str) -> int:
+        value = input(prompt).strip()
+        if not value.isdigit():
+            raise ValueError("Pozitív szám szükséges.")
+        return int(value)
+
+    @staticmethod
+    def _validate_positive_amount(amount: int):
+        if amount <= 0:
+            raise ValueError("Az összegnek pozitívnak kell lennie!")
+
---
+++ ui/admin_menu.py
@@ -0,0 +1,111 @@
+from ui.base_menu import BaseMenu
+from repositories.user_repository import UserRepository
+from datetime import datetime
+
+class AdminMenu(BaseMenu):
+
+    def show(self):
+        menu = {
+            "1": ("Új felhasználó hozzáadása", self.add_user),
+            "2": ("Felhasználók listázása", self.get_users),
+            "3": ("Felhasználó adatainak módosítása", self.edit_user),
+            "4": ("Felhasználó törlése", self.delete_user),
+            "0": ("Kilépés", self.exit_app)
+        }
+
+        self.run(menu, "Felhasználókezelés")
+
+    def add_user(self):
+        name = input("Név: ")
+        email = input("Email: ")
+        username = input("Felhasználónév: ")
+        password = input("Jelszó: ")
+        tmp = input("Szerepkör: (1)user (2)admin:")
+        role = "user" if tmp == "1" else "admin"
+
+        user_data = {
+            "name": name,
+            "email": email,
+            "username": username,
+            "password": password,
+            "role": role,
+            "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+        UserRepository.create_user(user_data)
+        print(f"Felhasználó sikeresen létrehozva. Név: {user_data['name']} ")
+
+    def get_users(self):
+        users = UserRepository.get_all()
+
+        if not users:
+            print("\nNincs rögzített felhasználó!")
+            return
+
+        print("\nFELHASZNÁLÓK")
+        print(
+            f"{'Név'.ljust(20)} | "
+            f"{'Email'.ljust(20)} | "
+            f"{'Szerepkör'.ljust(10)} | "
+            f"{'Felhasználónév'.ljust(20)} | "
+            f"{'Létrehozva'.ljust(20)} | "
+            f"{'Módosítva'.ljust(20)}"
+        )
+
+        for u in users:
+            print(
+                f"{u.get('name', '').ljust(20)} | "
+                f"{u.get('email', '').ljust(20)} | "
+                f"{u.get('role', '').ljust(10)} | "
+                f"{u.get('username', '').ljust(20)} | "
+                f"{u.get('createdAt', '').ljust(20)} | "
+                f"{u.get('modifiedAt', '-').ljust(20)}"
+            )
+
+    def edit_user(self):
+        username = input("Add meg a felhasználónevet: ")
+        user = UserRepository.find_by_username(username)
+
+        if not user:
+            print("Felhasználó nem található!")
+            return
+
+        print("\n--- Felhasználó adatainak módosítása ---")
+        print("(Enter = a megadott adat változatlan marad)\n")
+
+        name = input(f"Név [{user.get('name')}]: ").strip()
+        email = input(f"Email [{user.get('email')}]: ").strip()
+        role_tmp = input(f"Szerepkör: (1)user (2)admin [{user.get('role')}]: ").strip()
+        role = "user" if role_tmp == "1" else "admin"
+
+        updates = {}
+        if name:
+            updates["name"] = name
+        if email:
+            updates["email"] = email
+        if role:
+            updates["role"] = role
+
+        updates["username"] = username
+        updates["createdAt"] = user.get("createdAt")
+        updates["modifiedAt"] = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+
+        UserRepository.update_user(username, updates)
+        print("A felhasználó adatai sikeresen módosítva!")
+
+    def delete_user(self):
+        username = input("Add meg a törlendő felhasználó felhasználónevét: ")
+        user = UserRepository.find_by_username(username)
+
+        if not user:
+            print("Nincs ilyen regisztrált felhasználó.")
+            return False
+
+        tmp = input("Biztosan törölni szeretnéd a felhasználót? (I) Igen (N) Mégsem: ").lower()
+        if tmp=="i":
+            UserRepository.delete_user(username)
+            print("Felhasználó sikeresen törölve")
+            return True
+        else:
+            print("Felhasználó törlése megszakítva.")
+            return False
